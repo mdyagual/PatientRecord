c62e4fbeb47c7221813bc433e896d84a550a443d
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-06-02 15:53:18 -0500

    Changed to annotation Autowired in order to have another styling for injection dependency.

--- src/main/java/com/example/patientrecord/usecases/SavePatientUseCase.java
+++ src/main/java/com/example/patientrecord/usecases/SavePatientUseCase.java
@@ -5,21 +5,22 @@ import com.example.patientrecord.mapper.PatientMapper;
 import com.example.patientrecord.model.PatientDTO;
 import com.example.patientrecord.repository.IPatientRepository;
 import com.example.patientrecord.usecases.interfaces.SavePatient;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class SavePatientUseCase implements SavePatient {
-    private final IPatientRepository patientRepo;
-    private final PatientMapper patientMapper;
-
-    public SavePatientUseCase(IPatientRepository patientRepo, PatientMapper patientMapper) {
-        this.patientRepo = patientRepo;
-        this.patientMapper = patientMapper;
-    }
+    @Autowired
+    private IPatientRepository patientRepo;
+    @Autowired
+    private PatientMapper patientMapper;
 
     @Override
     public Mono<PatientDTO> apply(PatientDTO pdto){
-        return patientRepo.save(patientMapper.convertDTOToEntity().apply(pdto)).map(patient -> patientMapper.convertEntityToDTO().apply(patient));
+        return patientRepo
+                .save(patientMapper.convertDTOToEntity()
+                        .apply(pdto)).map(patient -> patientMapper.convertEntityToDTO().apply(patient));
     }
 }
