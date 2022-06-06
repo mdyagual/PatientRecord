6ee30c1b5ffb10247eb085016127f1d2495a8f77
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-06-06 17:11:04 -0500

    Swagger implementation

--- src/main/java/com/example/patientrecord/PatientRecordExampleApplication.java
+++ src/main/java/com/example/patientrecord/PatientRecordExampleApplication.java
@@ -3,8 +3,10 @@ package com.example.patientrecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@EnableSwagger2
 public class PatientRecordExampleApplication {
 
 	public static void main(String[] args) {
